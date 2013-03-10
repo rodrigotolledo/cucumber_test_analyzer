3f8e7aaede576f9608e0ed21297fd44b7692cd15
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-03-10 18:52:44 -0300

    fixing reports module.

--- reports_module.rb
+++ reports_module.rb
@@ -2,21 +2,28 @@
 
 require 'erb'
 
-puts ""
-puts "[Running reports_module.rb...]"
+module ReportsModule
+	class Reports
+		attr_accessor(:percentage_user_input, :total_of_existing_tests, :total_of_pending_tests, :percentage_of_pending_tests)
 
-ARGV.each do
-	@percentage_user_input = ARGV[0]
-	@total_of_existing_tests = ARGV[1]
-	@total_of_pending_tests = ARGV[2]
-	@percentage_of_pending_tests = ARGV[3]+"%"
+		def initialize
+			@percentage_user_input = percentage_user_input
+			@total_of_existing_tests = total_of_existing_tests
+			@total_of_pending_tests = total_of_pending_tests
+			@percentage_of_pending_tests = percentage_of_pending_tests
+		end
+
+		def convert_erb_to_html_file(param1, param2, param3, param4)
+			puts ""
+			puts "[Running reports_module.rb...]"
+			@percentage_user_input = param1
+			@total_of_existing_tests = param2
+			@total_of_pending_tests = param3
+			@percentage_of_pending_tests = param4
+			#TODO: generate reports in a specific reports folder
+			template_file = File.open("pending_tests_template.htm.erb", 'r').read
+			erb = ERB.new(template_file)
+			File.open("pending_tests_report.htm", 'w+') { |file| file.write(erb.result(binding)) }
+		end
+	end
 end
-
-def convert_erb_to_html_file
-	#TODO: generate reports in a specific reports folder
-	template_file = File.open("pending_tests_template.htm.erb", 'r').read
-	erb = ERB.new(template_file)
-	File.open("pending_tests_report.htm", 'w+') { |file| file.write(erb.result(binding)) }
-end
-
-convert_erb_to_html_file
