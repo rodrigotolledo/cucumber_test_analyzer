b32cb929e9170de9ca2f8cd58e3a5197373b6444
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-03-13 20:21:32 -0300

    adding method to generate html output for assertions module.

--- reports_module.rb
+++ reports_module.rb
@@ -2,28 +2,33 @@
 
 require 'erb'
 
-module ReportsModule
-	class Reports
-		attr_accessor(:percentage_user_input, :total_of_existing_tests, :total_of_pending_tests, :percentage_of_pending_tests)
+class Reports
 
-		def initialize
-			@percentage_user_input = percentage_user_input
-			@total_of_existing_tests = total_of_existing_tests
-			@total_of_pending_tests = total_of_pending_tests
-			@percentage_of_pending_tests = percentage_of_pending_tests
-		end
+	def print_user_message
+		puts ""
+		puts "[Running reports_module.rb...]"
+	end
 
-		def convert_erb_to_html_file(param1, param2, param3, param4)
-			puts ""
-			puts "[Running reports_module.rb...]"
-			@percentage_user_input = param1
-			@total_of_existing_tests = param2
-			@total_of_pending_tests = param3
-			@percentage_of_pending_tests = param4
-			#TODO: generate reports in a specific reports folder
-			template_file = File.open("pending_tests_template.htm.erb", 'r').read
-			erb = ERB.new(template_file)
-			File.open("pending_tests_report.htm", 'w+') { |file| file.write(erb.result(binding)) }
-		end
+	def generate_html_report_for_pending_tests(param1, param2, param3, param4)
+		print_user_message
+		@percentage_user_input = param1
+		@total_of_existing_tests = param2
+		@total_of_pending_tests = param3
+		@percentage_of_pending_tests = param4
+		#TODO: generate reports in a specific reports folder
+		template_file = File.open("pending_tests_template.htm.erb", 'r').read
+		erb = ERB.new(template_file)
+		File.open("pending_tests_report.htm", 'w+') { |file| file.write(erb.result(binding)) }
+	end
+
+	def generate_html_report_for_assertions(param1, param2, param3)
+		print_user_message
+		@quantity_of_assertions_user_input = param1
+		@total_of_existing_tests = param2
+		@total_of_assertions = param3
+		#TODO: generate reports in a specific reports folder
+		template_file = File.open("assertions_template.htm.erb", 'r').read
+		erb = ERB.new(template_file)
+		File.open("assertions_report.htm", 'w+') { |file| file.write(erb.result(binding)) }
 	end
 end
