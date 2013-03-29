cc7bdcbe2284ce4bfe279fb8311709789e334baf
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-03-29 19:02:18 -0300

    handling user input exceptions.

--- lib/user_module.rb
+++ lib/user_module.rb
@@ -12,26 +12,25 @@ Welcome to the Cucumber Tests Analyzer!
   [?] Help
 	"
 	print "> "
-	#TODO: add exception handler
-	#TODO: make menu more interactive (go and return + colors)
 	@option_selected = gets.chomp
+	case @option_selected
+	when "1" 
+		system("ruby start_pending_tests_module.rb")
+	when "2" 
+		system("ruby start_assertions_module.rb")
+	when "3" 
+		system("ruby start_slow_tests_module.rb")
+	when "4" 
+		puts "Placeholder for 4."
+	when "5" 
+		puts "Placeholder for 5."
+	when "?"
+		puts "Placeholder for Help."
+	else
+		system("clear")
+		puts "This option doesn't exist. Type '?' for help."
+		print_welcome_message
+	end
 end
 
 print_welcome_message
-
-case @option_selected
-when "1" 
-	system("ruby start_pending_tests_module.rb")
-when "2" 
-	system("ruby start_assertions_module.rb")
-when "3" 
-	system("ruby start_slow_tests_module.rb")
-when "4" 
-	puts "Placeholder for 4."
-when "5" 
-	puts "Placeholder for 5."
-when "?"
-	puts "Placeholder for Help."
-else
-	puts "This option doesn't exist. Type '?' for help."
-end
