e85c320355039b4945bd1b0a13acc3373ba3457b
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-03-30 23:22:58 -0300

    fixing parameters.

--- lib/reports_module.rb
+++ lib/reports_module.rb
@@ -9,33 +9,30 @@ class Reports
 		puts "[Running reports_module.rb...]"
 	end
 
-	def generate_html_report_for_pending_tests(param1, param2, param3, param4)
+	def generate_html_report_for_pending_tests(percentage_user_input, total_of_existing_tests, total_of_pending_tests, percentage_of_pending_tests)
 		print_user_message
-		@percentage_user_input = param1
-		@total_of_existing_tests = param2
-		@total_of_pending_tests = param3
-		@percentage_of_pending_tests = param4
-		#TODO: generate reports in a specific reports folder
+		@percentage_user_input = percentage_user_input
+		@total_of_existing_tests = total_of_existing_tests
+		@total_of_pending_tests = total_of_pending_tests
+		@percentage_of_pending_tests = percentage_of_pending_tests
 		template_file = File.open("../templates/pending_tests_template.htm.erb", 'r').read
 		erb = ERB.new(template_file)
 		File.open("../reports/pending_tests_report.htm", 'w+') { |file| file.write(erb.result(binding)) }
 	end
 
-	def generate_html_report_for_assertions(param1, param2, param3)
+	def generate_html_report_for_assertions(quantity_of_assertions_user_input, total_of_existing_tests, total_of_assertions)
 		print_user_message
-		@quantity_of_assertions_user_input = param1
-		@total_of_existing_tests = param2
-		@total_of_assertions = param3
-		#TODO: generate reports in a specific reports folder
+		@quantity_of_assertions_user_input = quantity_of_assertions_user_input
+		@total_of_existing_tests = total_of_existing_tests
+		@total_of_assertions = total_of_assertions
 		template_file = File.open("../templates/assertions_template.htm.erb", 'r').read
 		erb = ERB.new(template_file)
 		File.open("../reports/assertions_report.htm", 'w+') { |file| file.write(erb.result(binding)) }
 	end
 
-	def generate_html_report_for_slow_tests(param1)
+	def generate_html_report_for_slow_tests(scenarios_info)
 		print_user_message
-		@scenarios_info = param1
-		#TODO: generate reports in a specific reports folder
+		@scenarios_info = scenarios_info
 		template_file = File.open("../templates/slow_tests_template.htm.erb", 'r').read
 		erb = ERB.new(template_file)
 		File.open("../reports/slow_tests_report.htm", 'w+') { |file| file.write(erb.result(binding)) }
