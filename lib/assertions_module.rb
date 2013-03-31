c2ea54ca16230dbe357ea2046e076a836ab559d0
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-03-30 23:21:16 -0300

    handling user exceptions, refactory still needed.

--- lib/assertions_module.rb
+++ lib/assertions_module.rb
@@ -17,12 +17,32 @@ class Assertions < FileHandler
 		puts "What is considered to you as a maximum good quantity of assertions by test scenario? [default: 3]"
 		print "> "
 		@quantity_of_assertions_user_input = gets.chomp
-		#TODO: add exception handler
-		puts "What's the directory path of your step_definitions? [example: /Users/Rodrigo/myApp/step_definitions]"
-		print "> "
-		@step_definitions_path = gets.chomp
-		#TODO: add exception handler
-		puts ""
+		#TODO: fix regular expression to don't allow '00'
+		if not @quantity_of_assertions_user_input.match(/^((?!0$)[0-9]{1,2})$/)
+			system("clear")
+			puts "Invalid entry. Please, type a quantity of assertions between 1 and 99."
+			print_user_interaction
+		else
+			#TODO: refactory this code, it's the same thing of pending_tests_module.
+			puts "What's the directory path of your step_definitions? [example: /Users/Rodrigo/myApp/step_definitions]"
+			print "> "
+			@step_definitions_path = gets.chomp
+			if File.directory?(@step_definitions_path)
+				files = Dir.glob("**/*.rb")
+				while files.empty?
+					puts "Sorry, there isn't any step definitions (*.rb) files inside this path. Please type a valid path that contains your step definitions."
+					print "> "
+					@step_definitions_path = gets.chomp
+				end
+			else
+				while not File.directory?(@step_definitions_path)
+					puts "This path doesn't exist. Please type a valid path that contains your step_definitions."
+					print "> "
+					@step_definitions_path = gets.chomp
+				end
+			end
+			puts ""
+		end
 	end
 
 	def verify_how_many_assertions_exist
