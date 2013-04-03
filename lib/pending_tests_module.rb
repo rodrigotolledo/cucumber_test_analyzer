3966d89f9953edf710e4e20e9d02ea6abc15a00c
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-04-03 00:27:09 -0300

    refactoring methods.

--- lib/pending_tests_module.rb
+++ lib/pending_tests_module.rb
@@ -21,36 +21,53 @@ class PendingTests < FileHandler
 		return percentage_of_pending_tests
 	end
 
-	#TODO: Refactory this frankenstein...
-	def print_user_interaction
+	def print_first_message
 		puts ""
 		puts "[Running pending_tests_module.rb...]"
 		puts "What percentage of pending tests is considered harmful for you? [example: 10%, 20%, 50%, etc.]"
 		print "> "
+	end
+
+	def print_second_message
+		puts "What's the directory path of your step_definitions? [example: /Users/Rodrigo/myApp/step_definitions]"
+		print "> "
+	end
+
+	def read_and_validate_percentage_user_input
+		print_first_message
 		@percentage_user_input = gets.chomp
-		if not @percentage_user_input.match(/^(100|[0-9]{1,2})%$/)
+		if not @percentage_user_input.match(/^(100|[1-9][0-9]?)%$/)
 			system("clear")
-			puts "Invalid percentage. Examples of correct use: 5%, 23%, 50%, etc. "
-			print_user_interaction
-		else
-			puts "What's the directory path of your step_definitions? [example: /Users/Rodrigo/myApp/step_definitions]"
+			puts "Invalid percentage. Examples of correct use: 5%, 23%, 50%, etc."
+			read_and_validate_percentage_user_input
+		end
+	end
+
+	def read_and_validate_step_definitions_path
+		print_second_message
+		@step_definitions_path = gets.chomp
+		if(is_path_a_directory?)
+			files = Dir.glob("**/*.rb")
+		end
+		while files.empty?
+			puts "Sorry, there isn't any step definitions (*.rb) files inside this path. Please type a valid path that contains your step definitions."
 			print "> "
 			@step_definitions_path = gets.chomp
-			if File.directory?(@step_definitions_path)
-				files = Dir.glob("**/*.rb")
-				while files.empty?
-					puts "Sorry, there isn't any step definitions (*.rb) files inside this path. Please type a valid path that contains your step definitions."
-					print "> "
-					@step_definitions_path = gets.chomp
-				end
-			else
-				while not File.directory?(@step_definitions_path)
-					puts "This path doesn't exist. Please type a valid path that contains your step_definitions."
-					print "> "
-					@step_definitions_path = gets.chomp
-				end
-			end
 		end
+	end
+
+	def is_path_a_directory?
+		while not File.directory?(@step_definitions_path)
+			puts "This path doesn't exist. Please type a valid path that contains your step_definitions."
+			print "> "
+			@step_definitions_path = gets.chomp
+		end
+		return true
+	end
+
+	def print_user_interaction
+		read_and_validate_percentage_user_input
+		read_and_validate_step_definitions_path
 	end
 
 	def verify_how_many_pending_tests_exist
