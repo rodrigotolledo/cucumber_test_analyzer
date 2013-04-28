e767a39d830426148afe60c291147adbddf000cb
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-04-28 18:36:02 -0300

    organizing/deleting test files.

--- step_definitions/calculator/number_of_lines.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby -w
-
-File.open("addition2.feature") do |f|
-	f.slice_before(/^\s*Scenario:/) do |scenario|
-		title = scenario.shift.chomp
-		ct = scenario.map(&:strip).reject(&:empty?).size
-		puts "#{title} --> has #{ct} lines"
-	end
-end
