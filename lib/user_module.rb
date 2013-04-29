b07c535376f0e3b418073bcd6f6a3125c3279615
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-04-29 17:08:48 -0300

    adding help description to User Module.

--- lib/user_module.rb
+++ lib/user_module.rb
@@ -26,7 +26,12 @@ Welcome to the Cucumber Tests Analyzer!
 	when "5" 
 		system("ruby start_time_frame_tests_module.rb")
 	when "?"
-		puts "Placeholder for Help."
+		puts "Cucumber Test Analyzer - Help"
+		puts "Pending Tests Module - it will help you analyze percentage of pending tests."
+		puts "Assertions Module - it will help you analyze overall quantity of tests and assertions."
+		puts "Slow Tests Module - it will run each test and organize them by slowness."
+		puts "Extensive Tests Module - it will help to identify tests that have more steps than defined by user."
+		puts "Time Frame Tests Module - it will run each test and identify which of them are between a time frame defined by user."
 	else
 		system("clear")
 		puts "This option doesn't exist. Type '?' for help."
